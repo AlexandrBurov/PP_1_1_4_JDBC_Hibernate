9c393199aed185d2007299c86bfbf8faadf148cb
macbook <a.d.burov@mail.ru>
2022-10-07 18:29:18 +0400

    pom fix

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -16,6 +16,7 @@ public class Main {
         userService.saveUser("Name2", "LastName2", (byte) 25);
         userService.saveUser("Name3", "LastName3", (byte) 31);
         userService.saveUser("Name4", "LastName4", (byte) 38);
+        userService.saveUser("Name5", "LastName5", (byte) 50);
 
         userService.removeUserById(1);
         userService.getAllUsers();
